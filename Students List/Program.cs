0b92295871aecbe3d40f984459c6192d4bfa7bfa
AJAY LAMA <Q2043246@live.tees.ac.uk>
2024-10-06 17:11:00 +0100

    Add project files.

---
+++ Students List/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Students_List;
+
+
+static void DisplayStudentsList(List<Student> studentsList)
+{
+    foreach (var student in studentsList)
+        Console.WriteLine(student);
+}
+
+Console.WriteLine("Students List\n");
+
+var students = new List<Student>
+{
+    new Student("Smith", "John", 19, "Computer Science", 85.5, 2021),
+    new Student("Doe", "Jane", 20, "Business Administration", 78.2, 2020),
+    new Student("Brown", "Michael", 21, "Mechanical Engineering", 92.0, 2019),
+    new Student("Taylor", "Emily", 22, "Medicine", 88.1, 2018),
+    new Student("Wilson", "Andrew", 23, "History", 74.5, 2022),
+    new Student("Davis", "Laura", 24, "Psychology", 81.3, 2020),
+    new Student("Miller", "Oliver", 20, "Computer Science", 95.0, 2023),
+    new Student("Lee", "Sophia", 19, "Economics", 68.9, 2023),
+    new Student("Garcia", "Lucas", 21, "Mathematics", 79.5, 2021),
+    new Student("Martinez", "Olivia", 22, "Law", 84.0, 2019)
+};
+
+//DisplayStudentsList(students);
+
+
+
+
---
+++ Students List/Student.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Students_List
+{
+    public class Student
+    {
+        public Student()
+        {
+            Forename = "Unknown";
+            Surname = "Unknown";
+            Age = 0;
+            Course = "Unknown";
+            Mark = 0.0;
+            EnrolmentYear = 0;
+        }
+        public Student(string forename, string surname, int age, string course, double mark, int enrolementYear)
+        {
+            Forename = forename;
+            Surname = surname;
+            Age = age;
+            Course = course;
+            Mark = mark;
+            EnrolmentYear = enrolementYear;
+        }
+        public override string ToString()
+        {
+            return $"Name: {Forename} {Surname}, Age: {Age}, Course: {Course}, Mark: {Mark}, Enrolment Year: {EnrolmentYear}";
+        }
+
+        public string Forename { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public string Course { get; set; }
+        public double Mark { get; set; }
+        public int EnrolmentYear { get; set; }
+
+    }
+}
+
